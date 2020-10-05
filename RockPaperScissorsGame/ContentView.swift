5e65ea50afd7ef3ac3824995503c9f1ee0bac3c2
Eric Honan <erichonan@gmail.com>
2020-10-04 22:25:48 -0400

    Initial Commit

---
+++ RockPaperScissorsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsGame
+//
+//  Created by Eric Michael Honan on 10/4/20.
+//  Copyright © 2020 Eric Honan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
